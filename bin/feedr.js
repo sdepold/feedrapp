e0114425848c975011ac0ab08d1f96c3c800aa97
Sascha Depold <sascha@depold.com>
2015-08-20 21:38:00 +0200

    Respect PORT env variable

--- bin/feedr.js
+++ bin/feedr.js
@@ -6,8 +6,9 @@ require('babel/register');
 
 var Server = require('../src/server');
 var server = new Server();
+var port   = process.env.PORT || 8080;
 
-server.listen(8080, 'localhost', function () {
+server.listen(port, 'localhost', function () {
   var address = this.address();
 
   console.log('Feedr is running on http://%s:%s', address.address, address.port);
