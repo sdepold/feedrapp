4e209548286ef1faae8ef2359a08da67aeaa3fdc
Sascha Depold <sascha@depold.com>
2015-08-20 21:42:50 +0200

    Listen on different interface

--- bin/feedr.js
+++ bin/feedr.js
@@ -8,7 +8,7 @@ var Server = require('../src/server');
 var server = new Server();
 var port   = process.env.PORT || 8080;
 
-server.listen(port, 'localhost', function () {
+server.listen(port, '0.0.0.0', function () {
   var address = this.address();
 
   console.log('Feedr is running on http://%s:%s', address.address, address.port);
