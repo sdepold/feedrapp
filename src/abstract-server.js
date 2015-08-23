5a22c3fc79828cb8e05ad1ce448b8e093b2187f5
Sascha Depold <sascha@depold.com>
2015-08-23 13:57:01 +0200

    [Added] Logging of request summary

--- src/abstract-server.js
+++ src/abstract-server.js
@@ -4,6 +4,7 @@ import webPockets from 'web-pockets';
 export default class AbstractServer {
   constructor () {
     this.app = webPockets();
+    this.injectLogging();
     this.bindRoutes();
   }
 
@@ -16,6 +17,36 @@ export default class AbstractServer {
     if (this.httpServer) {
       this.httpServer.close(callback);
     }
+  }
+
+  injectLogging () {
+    function getStatusCode (result) {
+      if (result.body && (result.body.responseStatus !== result.statusCode)) {
+        return result.body.responseStatus;
+      } else {
+        return result.statusCode;
+      }
+    }
+
+    this.app.request.wrap('result', (result, request) => {
+      var start = new Date();
+
+      return result().then((result) => {
+        result.statusCode = getStatusCode(result);
+
+        console[result.statusCode < 500 ? 'log' : 'error']({
+          url: request.url,
+          host: request.headers.host,
+          success: result.statusCode < 400,
+          tags: ['result'],
+          statusCode: result.statusCode,
+          responseDetails: result.body.responseDetails,
+          duration: new Date() - start
+        });
+
+        return result;
+      });
+    });
   }
 
   bindRoutes () {
