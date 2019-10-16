d67d56c7374c057997e8c107890effd8b519b55f
Sascha Depold <sdepold@ebay.com>
2019-10-16 11:07:06 +0200

    Tweak ads

--- src/ads/index.js
+++ src/ads/index.js
@@ -5,13 +5,13 @@ const inMemory = require('./in-memory-ads');
 
 const selection = [
   {
-    title: 'Ad | Play-Doh Modeling Compound',
-    link: 'https://amzn.to/2lWpeo5',
-    content: '<img src="https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00JM5GW10&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mamaskind-20"> 10 cans of creative fun kids can get creative with just the right colors They need in this Play Doh 10 pack of 2 ounce cans',
-    contentSnippet: '10 cans of creative fun kids can get creative with just the right colors They need in this Play Doh 10 pack of 2 ounce cans',
+    title: 'Ad | Oculus Quest All-in-one VR Gaming Headset',
+    link: 'https://amzn.to/2J9POCH',
+    content: '<img src="https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07HNW68ZC&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mamaskind-20&language=en_US"> Oculus Quest is an all-in-one gaming system for virtual reality. No wires. No PC. Just set up with the Oculus mobile app, and you\'re free to explore VR from almost anywhere. Sensors inside the headset precisely track your movements and instantly translate them into VR, while helping you steer clear of nearby objects. And with Oculus Touch controllers, your hands are in the game, so you can see your every gesture and feel the power of every impact. Take VR gaming to the next level. And everywhere else. Setup requires an iPhone(iOS 10 or higher) or Android(6.0 Marshmallow or higher) smartphone, the Oculus App (free download), 802.11 b/g/n wireless Internet access, and an Oculus account.',
+    contentSnippet: 'Oculus Quest is an all-in-one gaming system for virtual reality. No wires. No PC. Just set up with the Oculus mobile app, and you\'re free to explore VR from almost anywhere. Sensors inside the headset precisely track your movements and instantly translate them into VR, while helping you steer clear of nearby objects. And with Oculus Touch controllers, your hands are in the game, so you can see your every gesture and feel the power of every impact. Take VR gaming to the next level. And everywhere else. Setup requires an iPhone(iOS 10 or higher) or Android(6.0 Marshmallow or higher) smartphone, the Oculus App (free download), 802.11 b/g/n wireless Internet access, and an Oculus account.',
     publishedDate: '2019-09-11T12:00:00.000Z',
     categories: [],
-    author: 'Play-Doh'
+    author: 'Oculus'
   },
   {
     title: 'Ad | All-new Echo (3rd Gen) - Smart speaker with Alexa - Twilight Blue',
@@ -21,6 +21,15 @@ const selection = [
     publishedDate: '2019-09-11T12:00:00.000Z',
     categories: [],
     author: 'Amazon'
+  },
+  {
+    title: 'Ad | PlayStation 4 Pro 1TB Console',
+    link: 'https://amzn.to/2BdNhmo',
+    content: '<img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01LOP8EZC&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mamaskind-20&language=en_US"> The most advanced PlayStation system ever. PS4 Pro is designed to take your favorite PS4 games and add to them with more power for graphics, performance, or features for your 4K HDR TV, or 1080p HD TV. Ready to level up?',
+    contentSnippet: 'The most advanced PlayStation system ever. PS4 Pro is designed to take your favorite PS4 games and add to them with more power for graphics, performance, or features for your 4K HDR TV, or 1080p HD TV. Ready to level up?    ',
+    publishedDate: '2019-09-11T12:00:00.000Z',
+    categories: [],
+    author: 'Sony'
   }
 ];
 const getAd = function () {
