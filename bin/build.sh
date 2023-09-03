cfafe8ea0a091dacc064c78fa1605bf89e4f3402
Sascha Depold <sascha@depold.com>
2023-09-03 14:24:16 +0200

    Add docker build script

---
+++ bin/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# bump package.json version to next minor
+npm version minor
+git push
+
+version=$(node -e "console.log(require('./package.json').version)")
+image_base_name=ghcr.io/sdepold/feedr
+image_name="$image_base_name:$version"
+
+# build the new docker images
+docker build --platform=linux/amd64 -t "$image_name" .
+
+# push images to registry
+docker push "$image_name"
