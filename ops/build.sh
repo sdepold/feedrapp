27a76c454b7f26dfe462c6e4f4702406a1257457
Sascha Depold <sascha@depold.com>
2023-09-10 17:08:16 +0200

    Move build script to root

---
+++ ops/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# start versioning
+pushd api
+npm version minor
+version=$(node -e "console.log(require('./package.json').version)")
+popd
+
+pushd web
+npm version $version
+popd
+
+git push
+git push --tags
+# end versioning
+
+# build the api
+pushd api
+docker build --platform=linux/amd64 -t "ghcr.io/sdepold/feedr-api:$version" .
+docker push "$ghcr.io/sdepold/feedr-api:$version"
+popd
+
+# build the web
+pushd web
+docker build --platform=linux/amd64 -t "ghcr.io/sdepold/feedr-web:$version" .
+docker push "ghcr.io/sdepold/feedr-web:$version"
+popd
