cabdfe23b62c5eb94b539482be033faf67abf0ad
Sascha Depold <sascha@depold.com>
2023-09-14 21:09:11 +0200

    Add tests

--- api/src/models/carbon-ads.js
+++ api/src/models/carbon-ads.js
@@ -18,21 +18,51 @@ async function getRawCarbonAd() {
   return ads[0];
 }
 
+function getAdLink(ad) {
+  return `https:${ad.statlink}?segment=placement:${PLACEMENT}`;
+}
+
+function getAdContent(ad) {
+  return `
+        <div id="carbonads">
+            <span>
+                <span class="carbon-wrap">
+                    <a href="${getAdLink(ad)}" class="carbon-img" target="_blank" rel="noopener sponsored">
+                        <img src="${ad.smallImage}" alt="ads via Carbon" border="0" height="100" width="130" style="max-width: 130px" />
+                    </a>
+                    <a href="${getAdLink(ad)}" class="carbon-text" target="_blank" rel="noopener sponsored">
+                        ${ad.description}
+                    </a>
+                </span>
+                <a
+                    href="http://carbonads.net/?utm_source=feedrappinfo&amp;utm_medium=ad_via_link&amp;utm_campaign=in_unit&amp;utm_term=carbon"
+                    class="carbon-poweredby"
+                    target="_blank"
+                    rel="noopener sponsored"
+                >
+                    ads via Carbon
+                </a>
+            </span>
+        </div>
+    `;
+}
+
+function formatCarbonAd(ad) {
+  return {
+    title: "Carbon Ad",
+    link: getAdLink(ad),
+    content: getAdContent(ad),
+    contentSnippet: ad.description,
+    publishedDate: new Date(Number(ad.timestamp) * 1000).toISOString(),
+    categories: [{ name: "ads" }],
+    author: "Carbon Ads",
+    thumbnail: ad.smallImage,
+  };
+}
+
 async function getCarbonAd() {
   try {
-    const ad = await getRawCarbonAd();
-    const link = `https:${ad.statlink}?segment=placement:${PLACEMENT}`;
-
-    return {
-      title: "Carbon Ad",
-      link,
-      content: ad.description,
-      contentSnippet: ad.description,
-      publishedDate: new Date(Number(ad.timestamp) * 1000).toISOString(),
-      categories: [{ name: "ads" }],
-      author: "Carbon Ads",
-      thumbnail: ad.smallImage,
-    };
+    return formatCarbonAd(await getRawCarbonAd());
   } catch (e) {
     console.log(e);
 
@@ -40,4 +70,4 @@ async function getCarbonAd() {
   }
 }
 
-module.exports = { getCarbonAd, getRawCarbonAd };
+module.exports = { getCarbonAd, formatCarbonAd, getAdContent, getRawCarbonAd };
---
+++ api/test/unit/models/carbon-ads.spec.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { formatCarbonAd, getAdContent } = require("../../../src/models/carbon-ads");
+
+describe("CarbonAds", () => {
+  describe("formatCarbonAd", () => {
+    it("formats carbon ads", () => {
+      const carbonAd = require("../fixture/carbon-ad.json");
+
+      expect(formatCarbonAd(carbonAd)).to.deep.equal({
+        title: "Carbon Ad",
+        link: "https://srv.buysellads.com/ads/click/x/GTND42J7CESIPK37CKSLYKQNF6BDEK3LF6YDKZ3JCYAIPKQNCK7DE27KCYBDEKJYCVSDC53MCKSD423YCKAILKQKC6SDL23UCT7IKK3EHJNCLSIZ?segment=placement:feedrappinfo",
+        content: getAdContent(carbonAd),
+        contentSnippet:
+          "Look forward to scrolling to find your errors. Speed. Precision. Performance. MX Master 3S Logitech",
+        publishedDate: "2023-09-14T15:30:47.000Z",
+        categories: [{ name: "ads" }],
+        author: "Carbon Ads",
+        thumbnail:
+          "https://cdn4.buysellads.net/uu/1/134924/1684244833-Copy-of-02_MX-Master-3S-260x200px.jpg",
+      });
+    });
+  });
+});
